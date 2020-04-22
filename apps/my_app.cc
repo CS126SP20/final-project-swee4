18d403ffe61e16f5a0f22334a5d0879617c59baf
swee4 <swee4@illinois.edu>
2020-04-22 10:29:41 -0500

    Incorporated External Library with Working Example
    Incorporate ciAnimatedGif CinderBlock to draw animated
    gifs on the Cinder display. Include ciAnimatedGif's header
    and source files are in the project as per direction
    in the external library's README file. Citations included.

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,6 +1,11 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include "my_app.h"
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
+#include "cinder/gl/Texture.h"
+#include "cinder/ImageIo.h"
+#include "../src/ciAnimatedGif.h"
 
 #include <cinder/app/App.h>
 
@@ -9,13 +14,24 @@ namespace myapp {
 
 using cinder::app::KeyEvent;
 
+    cinder::ciAnimatedGifRef mGif;
+
 MyApp::MyApp() { }
 
-void MyApp::setup() { }
+void MyApp::setup() {
+    mBackground_ = cinder::gl::Texture2d::create(loadImage(loadAsset("transparent-tree.png")));
+    mGif = cinder::ciAnimatedGif::create( loadAsset("cherry-blossom.gif"));
+}
 
 void MyApp::update() { }
 
-void MyApp::draw() { }
+void MyApp::draw() {
+    cinder::gl::clear();
+    // drawing background image to fit frame:
+    // https://libcinder.org/docs/guides/tour/hello_cinder_chapter1.html
+    cinder::gl::draw( mBackground_ ,getWindowBounds());
+    mGif->draw();
+}
 
 void MyApp::keyDown(KeyEvent event) { }
 
--- apps/my_app.h
+++ apps/my_app.h
@@ -4,6 +4,9 @@
 #define FINALPROJECT_APPS_MYAPP_H_
 
 #include <cinder/app/App.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
+#include "cinder/app/RendererGl.h"
 
 
 namespace myapp {
@@ -15,6 +18,8 @@ class MyApp : public cinder::app::App {
   void update() override;
   void draw() override;
   void keyDown(cinder::app::KeyEvent) override;
+private:
+    cinder::gl::Texture2dRef mBackground_;
 };
 
 }  // namespace myapp
